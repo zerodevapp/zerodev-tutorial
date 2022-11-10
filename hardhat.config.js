084d6cb1d8dc2416a48298f7a23ef1b80d6a6f59
Derek Chiang <me@derekchiang.com>
2022-11-09 19:38:40 -0500

    Deploy to Mumbai

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,21 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+require("@nomicfoundation/hardhat-toolbox")
 
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
 // testing the frontend.
-require("./tasks/faucet");
+require("./tasks/faucet")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.15",
   networks: {
     hardhat: {
       chainId: 1337 // We set 1337 to make interacting with MetaMask simpler
-    }
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_ID}`,
+      accounts: [process.env.MUMBAI_DEPLOYER_PRIVATE_KEY],
+    },
   }
-};
+}
